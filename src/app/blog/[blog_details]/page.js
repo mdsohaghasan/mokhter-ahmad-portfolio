6125173ddb4e7d21400b746fdbfe6bd657d6ea51
Sohag Hasan <mailsohaghasan@gmail.com>
2023-09-06 17:18:31 +0600

    delete

--- src/app/blog/[blog_details]/page.js
+++ src/app/blog/[blog_details]/page.js
@@ -2,49 +2,15 @@
 import Image from "next/image";
 import React from "react";
 import Link from "next/link";
-import BlogDetails from "./BlogDetails";
+import getBlogDetails from "./BlogDetails";
+
 
 
 const Page = async ({ params }) => {
  
-  const { blog_details } = params;
-  console.log(blog_details);
+  const { title, des, category, date, author, url } = await getBlogDetails(params.blog_details);  
 
-
-    
-  // const [Blog, setBlog] = useState([]);
-
-  // useEffect(() => {
-  //   const url = `https://test.mashqulquran.com/blog/${blog_details}`;
-  //   fetch(url, {
-  //     // headers: {
-  //     //   authorization: `Bearer ${token}`,
-  //     // },
-  //   })
-  //   .then((res) => res.json())
-  //   .then((data) => setBlog(data))
-  //   .catch((error) => {
-  //     console.error("Error fetching program data:", error);
-  //   });
-  // }, []);
-  // console.log(Blog);
-   
-
-  // const Blog = [
-  //   {
-  //     id: 1,
-  //     title: "Exploring the Essence of Islamic Spirituality: Insights and Reflections",
-  //     url: quran,
-  //     author: "mokhter ahmad",
-  //     date: "12-12-2022",
-  //   },
-  // ];
-
-  // console.log(Blog);
-
-// const [{ title, url, author, date }] = Blog;
-
-
+  console.log(title, des, category, date, author, url)
 
   return (
     <section id="blog">
@@ -53,7 +19,6 @@ const Page = async ({ params }) => {
         <div className="st-section-heading st-style1">
           <h4 className="st-section-heading-title">BLOG</h4>
           <h2 className="st-section-heading-subtitle">LATEST BLOG</h2>
-          {/* <p>{params}</p> */}
         </div>
         <div className="st-height-b25 st-height-lg-b25"></div>
       </div>
@@ -61,7 +26,31 @@ const Page = async ({ params }) => {
       <div className="lg:flex container gap-8">
 
 
-        <BlogDetails param={blog_details}></BlogDetails>
+      <div className="lg:basis-3/4 " >
+          
+          <div className="slick-slide-in" >
+            <div className="st-post-single st-style1">
+              <div  className="st-post-thumb st-zoom">
+                <Image src={url} className="st-zoom-in" alt="blog1" width={100} height={100} />
+              </div>
+              <div className="st-post-info">
+                <div className="st-post-date">
+                  By:
+                  <span className="st-post-author">
+                  {author}
+                  </span>
+                  <span className="st-post-date-divider">|</span>
+                  <span className="st-post-publish-date">{date}</span>
+                  <span className="st-post-date-divider">|</span>
+                  <span className="st-post-publish-date">{category}</span>
+                </div>
+                <h3 className="st-post-title">{title}</h3>
+                <h5 className="py-5">{des}</h5>
+              </div>
+            </div>
+          </div>
+          
+        </div>
 
         <div className="lg:basis-1/4  bg-[#070D1B] p-4">
           <div>
